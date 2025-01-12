298c4a52b0eb17e2889480769c4c26ca54048cd0
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-12 22:39:24 +0700

    untuk mengendalikan alur navigasi utama dalam aplikasi

---
+++ app/src/main/java/com/example/questfirebase/navigation/PengelolaHalaman.kt
@@ -0,0 +1,42 @@
+package com.example.questfirebase.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questfirebase.ui.home.pages.HomeScreen
+import com.example.questfirebase.ui.home.pages.InsertScreen
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertScreen(
+                onBack = {
+                    navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
