b6d22c861785da4cbf66b5645530a9e7e91ce087
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-12 21:14:24 +0700

    untuk mengelola data mahasiswa yang disimpan di Firebase Firestore

--- app/src/main/java/com/example/questfirebase/reporsitory/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/questfirebase/reporsitory/NetworkRepositoryMhs.kt
@@ -3,10 +3,10 @@ package com.example.questfirebase.reporsitory
 import com.example.questfirebase.model.mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 class NetworkRepositoryMhs(private val firestore: FirebaseFirestore
 ): RepositoryMhs {
@@ -26,19 +26,47 @@ class NetworkRepositoryMhs(private val firestore: FirebaseFirestore
         }
     }
 
-    override fun getMhs(nim: String): Flow<mahasiswa?> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<mahasiswa?> = callbackFlow {
+        val mhsDocument = firestore.collection("mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocument.remove()
+        }
     }
 
     override suspend fun insertMhs(mahasiswa: mahasiswa) {
-        TODO("Not yet implemented")
+        try{
+            firestore.collection("mahasiswa").add(mahasiswa).await()
+        }catch (e : Exception){
+            throw Exception("Gagal menambahkan dara mahasiswa :${e.message}")
+        }
     }
 
     override suspend fun deleteMhs(mahasiswa: mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        }catch (e : Exception){
+            throw Exception("Gagal menghapus data mahasiswa :${e.message}")
+        }
     }
 
     override suspend fun updateMhs(mahasiswa: mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        }catch (e: Exception){
+            throw Exception("Gagal mengupdate data mahasiswa :${e.message}")
+        }
     }
 }
