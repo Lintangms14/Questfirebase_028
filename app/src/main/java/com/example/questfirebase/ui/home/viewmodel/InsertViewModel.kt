cbfd53a173b82125a3a512c3f7297e399a805f7a
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-12 21:36:12 +0700

    untuk menyimpan status kesalahan pada form input

--- app/src/main/java/com/example/questfirebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/questfirebase/ui/home/viewmodel/InsertViewModel.kt
@@ -2,6 +2,20 @@ package com.example.questfirebase.ui.home.viewmodel
 
 import com.example.questfirebase.model.mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+){
+    fun isValid():Boolean{
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
