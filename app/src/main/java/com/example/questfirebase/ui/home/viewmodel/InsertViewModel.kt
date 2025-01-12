b13086bd5196881aa3206f12749cdbe38694cd5f
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-12 21:30:57 +0700

    untuk menyimpan dan memproses data mahasiswa dalam operasi tertentu, seperti input, pembaruan, atau penghapusan

---
+++ app/src/main/java/com/example/questfirebase/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.questfirebase.ui.home.viewmodel
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
