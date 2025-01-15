0217e2951b80490f2552149325711307256497a9
Lintangms14 <merayusukmalintang@gmail.com>
2025-01-15 15:43:35 +0700

    Menambahkan data pada model

--- app/src/main/java/com/example/questfirebase/model/mahasiswa.kt
+++ app/src/main/java/com/example/questfirebase/model/mahasiswa.kt
@@ -9,7 +9,10 @@ data class mahasiswa(
     val alamat : String,
     val jenisKelamin : String,
     val kelas : String,
-    val angkatan : String
+    val angkatan : String,
+    val judulSkripsi : String,
+    val dosenPembimbing1 : String,
+    val dosenPembimbing2: String
 ){
-    constructor() : this("", "", "", "", "", "")
+    constructor() : this("", "", "", "", "", "","","","")
 }
